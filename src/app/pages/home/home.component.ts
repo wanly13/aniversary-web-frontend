0d754ed5dedd06c331b10e8098668a47c0fb2478
wanly13 <wanlyobregon13@gmail.com>
2023-08-01 17:24:04 -0500

    condfi

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -23,6 +23,81 @@ export class HomeComponent {
       anio: "2022",
       imagen: "tres.mp4"
     },
+    {
+      dia: "6 de Noviembre",
+      anio: "2022",
+      imagen: "cuatro.jpeg"
+    },
+    {
+      dia: "11 de Noviembre",
+      anio: "2022",
+      imagen: "cinco.jpeg"
+    },
+    {
+      dia: "13 de Noviembre",
+      anio: "2022",
+      imagen: "seis.mp4"
+    },
+    {
+      dia: "6 de Noviembre",
+      anio: "2022",
+      imagen: "siete.jpeg"
+    },
+    {
+      dia: "11 de Noviembre",
+      anio: "2022",
+      imagen: "ocho.jpeg"
+    },
+    {
+      dia: "13 de Noviembre",
+      anio: "2022",
+      imagen: "nueve.mp4"
+    },  
+    {
+      dia: "6 de Noviembre",
+      anio: "2022",
+      imagen: "diez.jpeg"
+    },
+    {
+      dia: "11 de Noviembre",
+      anio: "2022",
+      imagen: "once.jpeg"
+    },
+    {
+      dia: "13 de Noviembre",
+      anio: "2022",
+      imagen: "doce.mp4"
+    },  
+    {
+      dia: "6 de Noviembre",
+      anio: "2022",
+      imagen: "trece.jpeg"
+    },
+    {
+      dia: "11 de Noviembre",
+      anio: "2022",
+      imagen: "catorce.jpeg"
+    },
+    {
+      dia: "13 de Noviembre",
+      anio: "2022",
+      imagen: "quince.mp4"
+    },  
+    {
+      dia: "6 de Noviembre",
+      anio: "2022",
+      imagen: "diesiseis.jpeg"
+    },
+    {
+      dia: "11 de Noviembre",
+      anio: "2022",
+      imagen: "diesisiete.jpeg"
+    },
+    {
+      dia: "13 de Noviembre",
+      anio: "2022",
+      imagen: "diesiocho.mp4"
+    },
     
   ]
 }
