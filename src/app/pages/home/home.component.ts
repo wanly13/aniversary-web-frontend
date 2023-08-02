f08235ed4839ecf3b31dbecd755680ff97a9e24d
wanly13 <wanlyobregon13@gmail.com>
2023-08-01 23:21:10 -0500

    mensajes

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -9,95 +9,113 @@ export class HomeComponent {
 
   data = [
     {
-      dia: "6 de Noviembre",
+      dia: "Cuando te conocí <3 ",
       anio: "2022",
-      imagen: "1.jpeg"
+      imagen: "1.jpeg",
+      mensaje: "Al conocerte, mi mundo cambió. Tu sonrisa se convirtió en mi mayor alegría."
     },
     {
-      dia: "11 de Noviembre",
+      dia: "",
       anio: "2022",
-      imagen: "2.jpeg"
+      imagen: "2.jpeg",
+      mensaje: "Tu mirada me envuelve en un sueño del que no quiero despertar. Contigo, todo es posible."
     },
     {
-      dia: "13 de Noviembre",
+      dia: "",
       anio: "2022",
-      imagen: "3.jpeg"
+      imagen: "3.jpeg",
+      mensaje: "Ese instante en que nuestras miradas se cruzaron, el tiempo se detuvo. Encontré en ti lo que buscaba."
     },
     {
-      dia: "6 de Noviembre",
+      dia: "",
       anio: "2022",
-      imagen: "4.jpeg"
+      imagen: "4.jpeg",
+      mensaje: "Cada palabra tuya es una melodía que hace latir mi corazón. Tu presencia llena mi ser de emoción."
     },
     {
-      dia: "11 de Noviembre",
+      dia: "",
       anio: "2022",
-      imagen: "5.jpeg"
+      imagen: "5.jpeg",
+      mensaje: "Como un cuento de hadas, llegaste a mi vida y la transformaste. Tu amor es mi magia personal."
     },
     {
-      dia: "13 de Noviembre",
+      dia: "Lo hermoso de estar contigo <3 ",
       anio: "2022",
-      imagen: "6.jpeg"
+      imagen: "6.jpeg",
+      mensaje: "A tu lado, el mundo se vuelve más hermoso. Eres el lienzo que pinta colores en mi vida."
     },
     {
-      dia: "6 de Noviembre",
+      dia: "",
       anio: "2022",
-      imagen: "7.jpeg"
+      imagen: "7.jpeg",
+      mensaje: "Cada minuto contigo es un tesoro que atesoro en mi corazón. Eres mi refugio en la tormenta."
     },
     {
-      dia: "11 de Noviembre",
+      dia: "",
       anio: "2022",
-      imagen: "8.jpeg"
+      imagen: "8.jpeg",
+      mensaje: "Juntos creamos momentos que se quedan grabados en el tiempo. Eres mi compañera de aventuras."
     },
     {
-      dia: "13 de Noviembre",
+      dia: "",
       anio: "2022",
-      imagen: "9.jpeg"
-    },  
-    {
-      dia: "6 de Noviembre",
-      anio: "2022",
-      imagen: "10.jpeg"
+      imagen: "9.jpeg",
+      mensaje: "Tu risa es mi canción favorita, tu abrazo mi lugar seguro. Eres mi razón para sonreír cada día."
     },
     {
-      dia: "11 de Noviembre",
+      dia: "",
       anio: "2022",
-      imagen: "11.jpeg"
+      imagen: "10.jpeg",
+      mensaje: "Explorando el mundo a tu lado, cada lugar se vuelve mágico. Eres mi cómplice en esta travesía."
     },
     {
-      dia: "13 de Noviembre",
+      dia: "Gracias por elegirme <3 ",
       anio: "2022",
-      imagen: "12.jpeg"
-    },  
-    {
-      dia: "6 de Noviembre",
-      anio: "2022",
-      imagen: "13.jpeg"
+      imagen: "11.jpeg",
+      mensaje: "Agradezco por cada sonrisa que me regalas, por cada día a tu lado. Eres mi fuente de felicidad constante."
     },
     {
-      dia: "11 de Noviembre",
+      dia: "",
       anio: "2022",
-      imagen: "14.jpeg"
+      imagen: "12.jpeg",
+      mensaje: "En cada desafío, encuentro en ti el apoyo que necesito. Eres mi fortaleza y mi paz interior."
     },
     {
-      dia: "13 de Noviembre",
+      dia: "",
       anio: "2022",
-      imagen: "15.jpeg"
-    },  
-    {
-      dia: "6 de Noviembre",
-      anio: "2022",
-      imagen: "16.jpeg"
+      imagen: "13.jpeg",
+      mensaje: "Tu amor ha llenado mi vida de significado. Agradezco por cada momento compartido."
     },
     {
-      dia: "11 de Noviembre",
+      dia: "",
       anio: "2022",
-      imagen: "17.jpeg"
+      imagen: "14.jpeg",
+      mensaje: "Tú y yo, un equipo imparable. Cada obstáculo se vuelve insignificante a tu lado."
     },
     {
-      dia: "13 de Noviembre",
+      dia: "",
       anio: "2022",
-      imagen: "18.jpeg"
+      imagen: "15.jpeg",
+      mensaje: "Eres el regalo más preciado que la vida me ha dado. Estoy agradecido por tenerte junto a mí."
     },
-    
+    {
+      dia: "Te prometo estar contigo siempre <3",
+      anio: "2022",
+      imagen: "16.jpeg",
+      mensaje: "Cada día a tu lado es un paso hacia un amor que durará para siempre. Nuestro destino es el infinito."
+    },
+    {
+      dia: "",
+      anio: "2022",
+      imagen: "17.jpeg",
+      mensaje: "Con el tiempo, nuestro amor se fortalece, nuestra unión se profundiza. Estaré contigo en cada amanecer."
+    },
+    {
+      dia: "",
+      anio: "2022",
+      imagen: "18.jpeg",
+      mensaje: "En esta historia que escribimos juntos, el final es solo el comienzo. Mi amor por ti será eterno."
+    }
   ]
+
 }
